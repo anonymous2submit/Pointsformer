0eaa141b3d79d45cd925976bde6097b51e0d3819
anonymous2submit <66791734+anonymous2submit@users.noreply.github.com>
2021-06-16 23:18:24 -0400

    Update __init__.py

--- classification/models/__init__.py
+++ classification/models/__init__.py
@@ -1,39 +1,4 @@
 from __future__ import absolute_import
-# from .util import sample_and_group
-from .pct import PCT
-from .Combine1 import combine1Amax
-from .Combine1 import combine1Cmax
-
-from .Combine12 import combine12Amax
-
-from .Combine1CUDA import combine1AmaxCUDA
-from .Combine1CUDA import combine1CmaxCUDA
-
-from .Combine1CUDA2 import combine1AmaxCUDA2
-
-from .pct2 import PCT2
-from .pct3 import PCT3
-from .pct4 import PCT4
-
-
-from .New1 import new1A, new1B, new1C, new1D, new1E, new1F
-from .Combine1CUDA3 import combine1AmaxCUDA3
-from .New2 import new2A
-from .New3 import new3A
-from .New4 import new4A, new4B, new4D
-from .New5 import new5A
-from .New6 import new6A
-
-
-from .local import modelA, modelB, modelC, modelD, modelE, modelF
-from .model2 import model2A, model2B, model2C, model2D, model2E, model2F, model2G
-from .model3 import model3A, model3B, model3C, model3D, model3E, model3F, model3G
-from .model4 import model4A
-from .model5 import model5A, model5B, model5C, model5D, model5E, model5F
-from .model6 import model6F, model6G
-from .model7 import model7H
-from .model8 import model8A, model8B, model8C, model8D, model8E, model8F, model8G, model8H
-from .model9 import model9A, model9B, model9C, model9D, model9E
 from .model10 import model10A, model10B, model10C, model10D, model10E, model10F, model10G, model10H, model10I
 from .model11 import model11A, model11B, model11C, model11D, model11E, model11F, model11G
 from .model12 import model12A_64, model12A_128, model12B_64, model12B_128, model12C_64, model12C_128, \
